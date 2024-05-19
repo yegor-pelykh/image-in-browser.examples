87231a8a0db35c971ffe50535e4bc759d0fb233e
Yegor Pelykh <yegor.dev@gmail.com>
2024-05-19 17:20:45 +0300

    Added examples of transformations

---
+++ examples/transform/copy-flip.ts
@@ -0,0 +1,44 @@
+/** @format */
+
+import { Utils } from '../_utils/utils.js';
+import { Folder } from '../_utils/folder.js';
+import { Section } from '../_utils/section.js';
+import {
+  FlipDirection,
+  Transform,
+  decodePng,
+  encodePng,
+} from 'image-in-browser';
+
+const inputFileName = 'buck_24.png';
+const outputFileName = 'buck_24_copyFlip.png';
+
+/**
+ * Flip image
+ */
+function testCopyFlipTransform() {
+  const input = Utils.readFile(Folder.input, Section.png, inputFileName);
+
+  // Decoding PNG bytes to MemoryImage
+  let image = decodePng({
+    data: input,
+  });
+
+  console.assert(image !== undefined);
+  if (image === undefined) return;
+
+  // Applying copyFlip filter
+  image = Transform.copyFlip({
+    image: image,
+    direction: FlipDirection.both,
+  });
+
+  // Encode MemoryImage to PNG bytes
+  const output = encodePng({
+    image: image,
+  });
+
+  Utils.writeFile(Folder.output, Section.png, outputFileName, output);
+}
+
+testCopyFlipTransform();
---
+++ examples/transform/copy-rotate.ts
@@ -0,0 +1,44 @@
+/** @format */
+
+import { Utils } from '../_utils/utils.js';
+import { Folder } from '../_utils/folder.js';
+import { Section } from '../_utils/section.js';
+import { Transform, decodePng, encodePng } from 'image-in-browser';
+
+const inputFileName = 'buck_24.png';
+const outputFileName = 'buck_24_copyFlip.png';
+
+/**
+ * Rotating an image
+ */
+function testCopyRotateTransform() {
+  const input = Utils.readFile(Folder.input, Section.png, inputFileName);
+
+  // Decoding PNG bytes to MemoryImage
+  let image = decodePng({
+    data: input,
+  });
+
+  console.assert(image !== undefined);
+  if (image === undefined) return;
+
+  // Ensuring the presence of an alpha channel
+  image = image.convert({
+    numChannels: 4,
+  });
+
+  // Applying copyRotate filter
+  image = Transform.copyRotate({
+    image: image,
+    angle: 45,
+  });
+
+  // Encode MemoryImage to PNG bytes
+  const output = encodePng({
+    image: image,
+  });
+
+  Utils.writeFile(Folder.output, Section.png, outputFileName, output);
+}
+
+testCopyRotateTransform();
